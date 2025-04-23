d947c78938afc27a6e8100f5999876ffd26aa1af
Daniel Bethell <bethelldaniel444@gmail.com>
2025-04-23 08:59:56 +0100

    Added comments

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ for i in range(runs):
     x_train_ood, y_train_ood, _, _, _, _ = dataset_manager.get_dataset(Datasets.CIFAR100)
 
     print(f"Training model...")
-    model = models.PosteriorModel(x_train=x_train_id, y_train=y_train_id, learning_rate=0.001)
+    model = models.HyperEvidentialModel(x_train=x_train_id, y_train=y_train_id, learning_rate=0.001)
 
     start_train_time = time.time()
     model.train(batch_size=64, epochs=250, verbose=0)
@@ -106,5 +106,5 @@ results_dict = {
     "eval_times": eval_times
 }
 
-with open("Results/cifar10_cifar100_postnet", "wb") as f:
+with open("Results/cifar10_cifar100_hedl", "wb") as f:
     pickle.dump(results_dict, f)
